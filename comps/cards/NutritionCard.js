7a08ddd601e65d89c32ec77ac46fd770b5d9de80
MaharlikaC <mclomata@my.bcit.ca>
2022-02-17 10:55:03 -0800

    Created NutritionCard + AmountBar

---
+++ comps/cards/NutritionCard.js
@@ -0,0 +1,86 @@
+/*
+Colors
+  Pitch: #000000
+  Dark: #242424
+  White: #FFFFFF
+  Grey: #B2B3C6
+  LightGreen: #DDF4C3
+  BrightGreen: #8BE22D
+  Orange: #FFA722
+  LightBlue: #DEEDF2
+  Red: #C12E2E
+*/
+
+
+import { useEffect, useState } from 'react';
+import {useRouter} from 'next/router';
+
+import styled from "styled-components";
+import AmountBar from "../global/Bar";
+
+
+const CardCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  width:100%;
+  min-height: 300px;
+  padding: 30px;
+  border-radius: 10px;
+  backgroung: #DAF7A6; 
+  box-shadow: 0px 4px 6px grey;
+`
+
+const TopCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 10px 0;
+`
+
+const Title = styled.label`
+  font-weight: bold;
+  font-size: 24px;
+  color: black;
+`
+const PrefLink = styled.label`
+  font-weight: light;
+  font-size: 14px;
+  color: blue;
+`
+const DataCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  space-between: 20px;
+`
+export default function InfoCard() {
+
+  const [nutrientOne, setNutrientOne] = useState("Nutrition 1");
+  const [nutrientTwo, setNutrientTwo] = useState("Nutrition 2");
+  const [nutrientThree, setNutrientThree] = useState("Nutrition 3");
+  const [nutrientFour, setNutrientFour] = useState("Nutrition 4");
+  const [nutrientFive, setNutrientFive] = useState("Nutrition 5");
+
+  const [valOne, setValOne] = useState(1);
+  const [valTwo, setValTwo] = useState(10);
+  const [valThree, setValThree] = useState(45);
+  const [valFour, setValFour] = useState(60);
+  const [valFive, setValFive] = useState(100);
+  
+
+  return (
+    <div>
+      <CardCont>
+        <TopCont>
+          <Title>Insights Based on Preferences</Title>
+          <PrefLink>customize preferences</PrefLink>
+        </TopCont>
+        <DataCont>
+          <AmountBar nutrient={nutrientOne} value={valOne}/>
+          <AmountBar nutrient={nutrientTwo} value={valTwo}/>
+          <AmountBar nutrient={nutrientThree} value={valThree}/>
+          <AmountBar nutrient={nutrientFour} value={valFour}/>
+          <AmountBar nutrient={nutrientFive} value={valFive}/>
+        </DataCont>
+      </CardCont>
+    </div>
+  )
+}
