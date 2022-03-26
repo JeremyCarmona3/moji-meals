dcbcbdde96bd23c3b240357676b920afb67136bd
Iori Takeshita <takeshitaiori@gmail.com>
2022-03-25 22:46:06 -0700

    changes

--- pages/mojilibrary.js
+++ pages/mojilibrary.js
@@ -9,39 +9,7 @@ import NavBar from '../comps/global/Navbar';
 import { 
   useTheme, 
   useData,
-  useNut,
-  useTSugar,
-  useCalo,
-  usePro,
-  useTotfat,
-  useTotfib,
-  useTotchol,
-  useTotcalc,
-  useTotsod,
-  useTotpot,
-  useTotvita,
-  useTotvitb6,
-  useTotvitb12,
-  useTotvitc,
-  useTotvitd,
-  useTotvite,
-  useTotvitk,
-  useTotthia,
-  useTotribo,
-  useTotnia,
-  useTotfolate,
-  useTotpanto,
-  useTotcholine,
-  useTotcopper,
-  useTotiron,
-  useTotmag,
-  useTotmang,
-  useTotphos,
-  useTotselen,
-  useTotsodium,
-  useTotzinc
-
-        } from "../utils/provider";
+  usePage} from "../utils/provider";
 import { bgcolor} from '../comps/variable';
 import ax from 'axios'
 
@@ -96,7 +64,8 @@ const InputCont = styled.div`
 width:100%;
 height:100%;
 display:flex;
-justify-content:space-around;
+flex-direction:column;
+justify-content:center;
 align-items:center;
 margin:10px;
 `
@@ -124,38 +93,8 @@ export default function Home({
   
   // const {setTheme} = useTheme();
   const {theme} =useTheme();
-  const {datalist,setDataList} =useData();
-  const {sbc,setSBC} =useCalo();
-  const {carbo,setCar} =useNut();
-  const {tsugar,setTSugar} = useTSugar();
-  const {pro, setPro} = usePro();
-  const {totfat, setTotfat} = useTotfat();
-  const {totfib, setTotfib} = useTotfib();
-  const {totchol, setTotchol} = useTotchol();
-  const {totcalc, setTotcalc} = useTotcalc();
-  const {totsod, setTotsod} = useTotsod();
-  const {totpot, setTotpot} = useTotpot();
-  const {totvita, setTotvita} = useTotvita();
-  const {totvitb6, setTotvitb6} = useTotvitb6();
-  const {totvitb12, setTotvitb12} = useTotvitb12();
-  const {totvitc, setTotvitc} = useTotvitc();
-  const {totvitd, setTotvitd} = useTotvitd();
-  const {totvite, setTotvite} = useTotvite();
-  const {totvitk, setTotvitk} = useTotvitk();
-  const {totthia, setTotthia} = useTotthia();
-  const {totribo, setTotribo} = useTotribo();
-  const {totnia, setTotnia} = useTotnia();
-  const {totfolate, setTotfolate} = useTotfolate();
-  const {totpanto, setTotpanto} = useTotpanto();
-  const {totcholine, setTotcholine} = useTotcholine();
-  const {totcopper, setTotcopper} = useTotcopper();
-  const {totiron, setTotiron} = useTotiron();
-  const {totmag, setTotmag} = useTotmag();
-  const {totmang, setTotmang} = useTotmang();
-  const {totphos, setTotphos} = useTotphos();
-  const {totselen, setTotselen} = useTotselen();
-  const {totsodium, setTotsodium} = useTotsodium();
-  const {totzinc, setTotzinc} = useTotzinc();
+  const {datalist,setDataList} =useData(Emoji);
+  const {page,setPage} =usePage(false);
 
 
   // var lists =[]
@@ -171,8 +110,8 @@ export default function Home({
   const [nutFactOne,setnutFactOne] = useState('name')
   const [nutFactTwo,setnutFactTwo] = useState('name')
   const [nutFactThree,setnutFactThree] = useState('name')
-
   const [fruit,setFruit] = useState('lemon')
+  const [alert,setAlert] = useState('')
 
   const [sbr_type,setSBRType] =useState('asc')
   // const [count,setCount] =useState(1)
@@ -249,7 +188,17 @@ export default function Home({
   } 
 
   const filter =() =>{
-    console.log("async call");
+
+    if(page == false){
+      setPage(true);
+      return setAlert("Please go to the setting page and choose your nutrition choice")
+    }
+
+    if(page == true){
+      setAlert("")
+    }
+
+
     setSBRType(sbr_type === 'asc' ? 'desc':'asc')
     setLists(datalist)
     if(sbr_type === 'desc'){
@@ -271,8 +220,10 @@ if(status == 'main'){
           </UserGuide>
           <InputCont>
             <TextInput onChange={(e)=>inputFilter(e.target.value)}></TextInput>
+            <button style ={{backgroundColor:sbr_type == "asc"? 'pink':'white'}} onClick={()=>filter()}>{sbr_type == "asc"? 'Asc':'Desc'}</button>
+            <div><h2>{alert}</h2></div>
           </InputCont>
-          <button style ={{backgroundColor:sbr_type == "asc"? 'pink':'white'}} onClick={()=>filter()}>Asc desc</button>
+        
           {lists.map((o,i)=><EmojiCard id = {o.name} key={i} emoji ={o.emoji} onclick ={()=>ShowDetails(i)}></EmojiCard>)} 
         </CardCont>
    </>)
