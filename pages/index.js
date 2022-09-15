0dace0de8a3f3cb8a055dbd6096397846c6d86bb
JeremyCarmona3 <jcarmona1@my.bcit.ca>
2022-09-15 13:23:16 -0700

    image bug

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import Image from 'next/image';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 import SignCard from '../comps/cards/SignCard';
 
-import { useTheme } from "../utils/provider";
-import {bgcolor} from '../comps/variable';
+import { useTheme } from '../utils/provider';
+import { bgcolor } from '../comps/variable';
 import axios from 'axios';
 import CreateProfileCard from '../comps/cards/CreateProfileCard';
 
@@ -16,7 +16,7 @@ const Cont = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color: #DDF4C3;
+  background-color: #ddf4c3;
 `;
 
 const RowCont = styled.div`
@@ -39,105 +39,107 @@ const ImgCont = styled.div`
 `;
 
 const Heading = styled.h1`
-  color: #7FC236;
+  color: #7fc236;
   font-size: 50px;
 `;
 
-export default function Landing({
-  
-}) { 
+export default function Landing({}) {
   const router = useRouter();
 
-  const [login, setLogin] = useState()
+  const [login, setLogin] = useState();
 
   const SignIn = () => {
-    setLogin(true)
-  }
+    setLogin(true);
+  };
 
   const SignUp = () => {
-    setLogin(false)
-  }
+    setLogin(false);
+  };
 
-  const [userFName, setUserFname] = useState(null)
-  const [userLName, setUserLname] = useState(null)
-  const [userEmail, setUserEmail] = useState(null)
-  const [userPassword, setUserPassword] = useState(null)
+  const [userFName, setUserFname] = useState(null);
+  const [userLName, setUserLname] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
+  const [userPassword, setUserPassword] = useState(null);
 
   const HandleFName = (value) => {
-    console.log(value)
-    setUserFname(value)
-  }
+    console.log(value);
+    setUserFname(value);
+  };
 
   const HandleLName = (value) => {
-    console.log(value)
-    setUserLname(value)
-  }
+    console.log(value);
+    setUserLname(value);
+  };
 
   const HandleEmail = (value) => {
-    console.log(value)
-    setUserEmail(value)
-  }
+    console.log(value);
+    setUserEmail(value);
+  };
 
   const HandlePassword = (value) => {
-    console.log(value)
-    setUserPassword(value)
-  }
+    console.log(value);
+    setUserPassword(value);
+  };
 
   const CreateProfile = () => {
     const newUser = {
       firstname: userFName,
       lastname: userLName,
       email: userEmail,
-      password: userPassword
-    }
+      password: userPassword,
+    };
 
-    axios.post('https://moji-meal-backend.herokuapp.com/signup', newUser)
-    .then((res) => {
-      if (res) {
-        localStorage.setItem('firstname', res.data.firstname)
-        localStorage.setItem('lastname', res.data.lastname)
-        localStorage.setItem('email', res.data.email)
+    axios
+      .post('https://moji-meal-backend.herokuapp.com/signup', newUser)
+      .then((res) => {
+        if (res) {
+          localStorage.setItem('firstname', res.data.firstname);
+          localStorage.setItem('lastname', res.data.lastname);
+          localStorage.setItem('email', res.data.email);
 
-        setUserFname(res.data.firstname)
-        setUserLname(res.data.lastname)
-        setUserEmail(res.data.email)
-        setUserPassword(res.data.password)
+          setUserFname(res.data.firstname);
+          setUserLname(res.data.lastname);
+          setUserEmail(res.data.email);
+          setUserPassword(res.data.password);
 
-        router.push('/mojilibrary')
-      }
-    }).catch((e) => {
-      console.log(e)
-      alert('Profile not created')
-    })
-  }
+          router.push('/mojilibrary');
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        alert('Profile not created');
+      });
+  };
 
   const UserSignin = () => {
     const newUser = {
       firstname: userFName,
       lastname: userLName,
       email: userEmail,
-      password: userPassword
-    }
+      password: userPassword,
+    };
 
-    axios.post('https://moji-meal-backend.herokuapp.com/login', newUser)
-    .then((res) => {
-      if (res) {
-        localStorage.setItem('firstname', res.data.firstname)
-        localStorage.setItem('lastname', res.data.lastname)
-        localStorage.setItem('email', res.data.email)
+    axios
+      .post('https://moji-meal-backend.herokuapp.com/login', newUser)
+      .then((res) => {
+        if (res) {
+          localStorage.setItem('firstname', res.data.firstname);
+          localStorage.setItem('lastname', res.data.lastname);
+          localStorage.setItem('email', res.data.email);
 
-        setUserFname(res.data.firstname)
-        setUserLname(res.data.lastname)
-        setUserEmail(res.data.email)
-        setUserPassword(res.data.password)
+          setUserFname(res.data.firstname);
+          setUserLname(res.data.lastname);
+          setUserEmail(res.data.email);
+          setUserPassword(res.data.password);
 
-        router.push('/mojilibrary')
-      }
-    }).catch((e) => {
-      console.log(e)
-      alert('Not signed in')
-    })
-  }
+          router.push('/mojilibrary');
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        alert('Not signed in');
+      });
+  };
 
   if (login === false) {
     return (
@@ -146,22 +148,35 @@ export default function Landing({
           <ColCont>
             <Heading>Welcome To</Heading>
             <ImgCont>
-              <Image src='/Logo.PNG' alt="Moji Meals Logo" width={484} height={243} />
+              <Image
+                src='/Logo.png'
+                alt='Moji Meals Logo'
+                width={484}
+                height={243}
+              />
             </ImgCont>
           </ColCont>
           <ColCont>
-            <CreateProfileCard 
+            <CreateProfileCard
               spanOnClick={SignIn}
               btnOnClick={CreateProfile}
-              fnameInput={(e)=>{HandleFName(e.target.value)}}
-              lnameInput={(e)=>{HandleLName(e.target.value)}}
-              emailInput={(e)=>{HandleEmail(e.target.value)}}
-              passwordInput={(e)=>{HandlePassword(e.target.value)}}
+              fnameInput={(e) => {
+                HandleFName(e.target.value);
+              }}
+              lnameInput={(e) => {
+                HandleLName(e.target.value);
+              }}
+              emailInput={(e) => {
+                HandleEmail(e.target.value);
+              }}
+              passwordInput={(e) => {
+                HandlePassword(e.target.value);
+              }}
             />
           </ColCont>
         </RowCont>
       </Cont>
-    )
+    );
   } else {
     return (
       <Cont>
@@ -169,23 +184,32 @@ export default function Landing({
           <ColCont>
             <Heading>Welcome To</Heading>
             <ImgCont>
-              <Image src='/Logo.PNG' alt="Moji Meals Logo" width={484} height={243} />
+              <Image
+                src='/Logo.png'
+                alt='Moji Meals Logo'
+                width={484}
+                height={243}
+              />
             </ImgCont>
           </ColCont>
           <ColCont>
-            <SignCard 
+            <SignCard
               spanOnClick={SignUp}
               btnOnClick={UserSignin}
               headingtext='Sign In'
               endlabeltext="Don't have an account?"
               textspantext='Sign Up'
               btntext='Sign In'
-              emailInput={(e)=>{HandleEmail(e.target.value)}}
-              passwordInput={(e)=>{HandlePassword(e.target.value)}}
+              emailInput={(e) => {
+                HandleEmail(e.target.value);
+              }}
+              passwordInput={(e) => {
+                HandlePassword(e.target.value);
+              }}
             />
           </ColCont>
         </RowCont>
       </Cont>
-    )
+    );
   }
 }
